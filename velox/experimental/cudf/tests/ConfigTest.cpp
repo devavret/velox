5ed9557a393148064d878ec7f78d7f768ccbfa9f
Matt Gara <mgara@nvidia.com>
2026-08-26 16:10:24 -0700

    feat(ucx): Add adaptive GPU exchange compression

--- velox/experimental/cudf/tests/ConfigTest.cpp
+++ velox/experimental/cudf/tests/ConfigTest.cpp
@@ -1,35 +1,15 @@
-/*
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include "velox/experimental/cudf/CudfConfig.h"
-
-#include <gtest/gtest.h>
-
-namespace facebook::velox::cudf_velox::test {
-
-TEST(ConfigTest, cudfConfig) {
-  std::unordered_map<std::string, std::string> options = {
-      {CudfConfig::kCudfEnabled, "false"},
-      {CudfConfig::kCudfDebugEnabled, "true"},
-      {CudfConfig::kCudfMemoryResource, "arena"},
-      {CudfConfig::kCudfMemoryPercent, "25"},
-      {CudfConfig::kCudfFunctionNamePrefix, "presto"},
-      {CudfConfig::kCudfStreamingGroupbyEnabled, "true"},
-      {CudfConfig::kCudfStreamingGroupbyCapacityMultiplier, "3.5"},
-      {CudfConfig::kCudfAllowCpuFallback, "false"}};
+      {CudfConfig::kCudfAllowCpuFallback, "false"},
+      {CudfConfig::kUcxExchange, "true"},
+      {CudfConfig::kUcxxErrorHandling, "false"},
+      {CudfConfig::kUcxIntraNodeExchange, "true"},
+      {CudfConfig::kUcxxBlockingPolling, "false"},
+      {CudfConfig::kUcxExchangeLogLevel, "2"},
+      {CudfConfig::kUcxPartitionedOutputBatchRows, "100000"},
+      {CudfConfig::kUcxExchangeCompression, "column-adaptive-freq-pfor-min128"},
+      {CudfConfig::kUcxExchangeCompressionPipeline, "true"},
+      {CudfConfig::kUcxExchangeCompressionPipelineThreads, "2"},
+      {CudfConfig::kUcxExchangeCompressionMinBytes, "268435456"},
+      {CudfConfig::kUcxExchangeCompressionSafetyMargin, "1.5"}};
 
   CudfConfig config;
   ASSERT_FALSE(config.streamingGroupbyEnabled);
@@ -43,5 +23,16 @@ TEST(ConfigTest, cudfConfig) {
   ASSERT_EQ(config.streamingGroupbyEnabled, true);
   ASSERT_EQ(config.streamingGroupbyCapacityMultiplier, 3.5);
   ASSERT_EQ(config.allowCpuFallback, false);
+  ASSERT_TRUE(config.exchange);
+  ASSERT_FALSE(config.ucxxErrorHandling);
+  ASSERT_TRUE(config.intraNodeExchange);
+  ASSERT_FALSE(config.ucxxBlockingPolling);
+  ASSERT_EQ(config.exchangeLogLevel, 2);
+  ASSERT_EQ(config.partitionedOutputBatchRows, 100000);
+  ASSERT_EQ(config.exchangeCompression, "column-adaptive-freq-pfor-min128");
+  ASSERT_TRUE(config.exchangeCompressionPipeline);
+  ASSERT_EQ(config.exchangeCompressionPipelineThreads, 2);
+  ASSERT_EQ(config.exchangeCompressionMinBytes, 268435456);
+  ASSERT_DOUBLE_EQ(config.exchangeCompressionSafetyMargin, 1.5);
 }
 } // namespace facebook::velox::cudf_velox::test
